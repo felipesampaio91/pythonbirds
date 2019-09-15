e95fa53cbb6a3974ffa39d15e403e387785881a6
Felipe Sampaio <felipe.sampaio91@gmail.com>
2019-09-15 04:11:03 -0300

    Criado atributo de classe olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,10 @@
-class Pessoa:
+class Pessoa:# Cria a classe pessoa
+    olhos = 2 # Atributo de classe
+
     def __init__(self, *filhos, nome=None, idade=28):
-        self.idade = idade
-        self.nome = nome
-        self.filhos = list(filhos)
+        self.idade = idade # Atributo idade
+        self.nome = nome # Atributo nome
+        self.filhos = list(filhos) # Atributo filhos
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
@@ -15,10 +17,15 @@ if __name__ == '__main__':
     print(antonio.cumprimentar())
     print(antonio.nome)
     print(antonio.idade)
-    for filho in antonio.filhos:
+    for filho in antonio.filhos:# atributo dinâmico
         print(filho.nome)
     antonio.sobrenome = 'Oliveira'
-    del antonio.filhos
-    print(antonio.__dict__)
-    print(felipe.__dict__)
+    del antonio.filhos # Remoção do atributo dinâmico
+    print(antonio.__dict__)# Mostra os atributos de instância do objeto
+    print(felipe.__dict__)# Mostra os atributos de instância do objeto
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(antonio.olhos)
+    print(felipe.olhos)
+    print(id(Pessoa.olhos), id(antonio.olhos), id(felipe.olhos) )
 
