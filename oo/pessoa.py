906ebb031c1ddb2b54f60d62a28b6a92abb4b8b5
Felipe Sampaio <felipe.sampaio91@gmail.com>
2019-09-24 19:54:44 -0300

    Criado atributo de classe olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,8 +6,15 @@ class Pessoa:# Cria a classe pessoa
         self.nome = nome # Atributo nome
         self.filhos = list(filhos) # Atributo filhos
 
-    def cumprimentar(self):
+    def cumprimentar(self):# Método cumprimentar
         return f'Olá {id(self)}'
+
+    @staticmethod #Decorator
+    def metodo_estatico(): #Metodo de classe
+        return 50
+    @classmethod #Decorator
+    def nomes_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     felipe = Pessoa(nome='Felipe')
@@ -28,4 +35,6 @@ if __name__ == '__main__':
     print(antonio.olhos)
     print(felipe.olhos)
     print(id(Pessoa.olhos), id(antonio.olhos), id(felipe.olhos) )
+    print(Pessoa.metodo_estatico(), felipe.metodo_estatico())
+    print(Pessoa.nomes_e_atributos_de_classe(), felipe.nomes_e_atributos_de_classe())
 
